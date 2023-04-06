59dcabbbd71a8ac277b82426fe786f553233ba3b
Frank1503 <frank.rinkens@live.nl>
2023-04-06 15:17:23 +0200

    opracht-1: Oplossing

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -9,8 +9,8 @@ public class App {
     public static void main(String[] args) throws SQLException {
         PersonRepository repository = new PersonRepository();
 
-        //TODO 1c gebruik de repository om de person op te halen en print deze.
-        // Vul hier de primary key in van het record in de person tabel.
+        Person person = repository.readPerson(1);
+        System.out.println(person);
 
     }
 }
--- src/main/java/repository/PersonRepository.java
+++ src/main/java/repository/PersonRepository.java
@@ -1,33 +1,39 @@
 package repository;
 
+import domain.Gender;
 import domain.Person;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class PersonRepository {
     private static final String URL = "jdbc:postgresql://localhost:5432/jpa";
     private static final String USER_NAME = "postgres";
     private static final String PASSWORD = "mysecretpassword";
 
-    //TODO Opdracht 1: In deze opdracht gaan we een person uit de database lezen
-    // op basis van de primary key die we binnen krijgen
     public Person readPerson(int primaryKey) throws SQLException {
         Person person = null;
 
-        //TODO 1a maak een query die persoon ophaalt.
-        // Als het goed is, is dit de person die je in de voorbereiding zelf hebt toegevoegd
-        String sql = "";
+        String sql = "select * from person p where p.id = ?";
 
         try (
                 Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
                 PreparedStatement preparedStatement = connection.prepareStatement(sql)
         ) {
-            //TODO 1b haal de data op en maak een Person object met plain java (denk aan het JDBC hoofdstuk van je OCP)
-        }
+            preparedStatement.setInt(1, primaryKey);
 
-        return person;
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt(1);
+                    String firstName = resultSet.getString(2);
+                    String lastName = resultSet.getString(3);
+                    LocalDate dateOfBirth = resultSet.getObject(4, LocalDate.class);
+                    Gender gender = Gender.valueOf(resultSet.getString(5));
+
+                    person = new Person(id, firstName, lastName, dateOfBirth, gender);
+                }
+            }
+            return person;
+        }
     }
 }
