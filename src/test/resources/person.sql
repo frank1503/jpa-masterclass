11058de571d1ab84a30169722c774b1e2c507ef7
Frank1503 <frank.rinkens@live.nl>
2023-04-08 14:45:07 +0200

    opdracht 4: fix naming of columns

--- src/test/resources/person.sql
+++ src/test/resources/person.sql
@@ -1,4 +1,4 @@
 insert into jpa.public.person
-(id, first_name, last_name, date_of_birth, gender, street_name, house_number, zip_code, city, country)
+(id, firstname, lastname, dateofbirth, gender, streetname, housenumber, zipcode, city, country)
 values
     (1, 'Frank', 'Rinkens', '1986-03-15', 'MALE', 'Dolphijnstraat', '10', '5632CZ', 'Eindhoven', 'Nederland');
