0b52a8e69dea60f2740b59cff6aadc25d9c51b72
Frank1503 <frank.rinkens@live.nl>
2023-04-16 12:00:25 +0200

    Revert "opdracht 7: geen entityListener"
    This reverts commit b0f2791be1825ebde8c7953992e5244ca46a0981.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 
+@EntityListeners(AuditTrailerListener.class)
 @Entity
 @IdClass(CarPK.class)
 public class Car {
