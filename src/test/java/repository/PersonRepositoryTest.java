60972105d2c5c46132138658670c7ddcaea934fa
Frank1503 <frank.rinkens@live.nl>
2023-04-18 20:16:53 +0200

    opdracht 3: unit test

--- src/test/java/repository/PersonRepositoryTest.java
+++ src/test/java/repository/PersonRepositoryTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PersonRepositoryTest {
     private final PersonRepository repository = new PersonRepository();
