b5b2d9380b45d6e85a37173d912a7b794fb84b99
Frank1503 <frank.rinkens@live.nl>
2023-04-18 20:12:26 +0200

    opdracht 9: unit test

--- src/test/java/jpa/repository/CarRepositoryTest.java
+++ src/test/java/jpa/repository/CarRepositoryTest.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 //TODO 9d draai de unit tests. Deze moeten slagen
 @ExtendWith(SpringExtension.class)
