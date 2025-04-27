48a82ddf98b739d3875b5ce1bcdf52b770c3cc39
FrankRinkens <frank.rinkens@group9.nl>
2025-04-27 09:43:54 +0200

    opdracht-5: pas test aan

--- src/test/resources/insert-person.sql
+++ src/test/resources/insert-person.sql
@@ -1,4 +1,4 @@
 insert into jpa.public.person
 (id, firstname, lastname, dateofbirth, gender)
 values
-    (1, 'Frank', 'Rinkens', '1986-03-15', 'MALE');
+(1, 'Frank', 'Rinkens', '1986-03-15', 'MALE');
