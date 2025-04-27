cdd47d0228560cfd4919ac2d428e844967bdfa64
FrankRinkens <frank.rinkens@group9.nl>
2025-04-27 09:47:00 +0200

    opdracht-5: pas test aan

--- src/test/java/repository/PersonRepositoryTest.java
+++ src/test/java/repository/PersonRepositoryTest.java
@@ -13,25 +13,25 @@ class PersonRepositoryTest {
 
     @Test
     void shouldCreateAndReadPerson() {
-        Person person = new Person(2, "Rick", "Roelofsen", LocalDate.parse("1986-03-15"), Gender.MALE);
+        Person person = new Person(2, "John", "Johnson", LocalDate.parse("1990-04-26"), Gender.MALE);
         repository.createPerson(person);
         Person createdPerson = repository.readPerson(2);
         assertThat(createdPerson).isNotNull();
-        assertThat(createdPerson.getFirstName()).isEqualTo("Rick");
-        assertThat(createdPerson.getLastName()).isEqualTo("Roelofsen");
-        assertThat(createdPerson.getDateOfBirth()).isEqualTo(LocalDate.parse("1986-03-15"));
+        assertThat(createdPerson.getFirstName()).isEqualTo("John");
+        assertThat(createdPerson.getLastName()).isEqualTo("Johnson");
+        assertThat(createdPerson.getDateOfBirth()).isEqualTo(LocalDate.parse("1990-04-26"));
         assertThat(createdPerson.getGender()).isEqualTo(Gender.MALE);
     }
 
     @Test
     void shouldUpdatePerson() {
-        Person person = new Person(2, "Willy", "Roelofsen", LocalDate.parse("1986-03-15"), Gender.MALE);
+        Person person = new Person(2, "Willy", "Johnson", LocalDate.parse("1990-04-26"), Gender.MALE);
         repository.updatePerson(person);
         Person updatedPerson = repository.readPerson(2);
         assertThat(updatedPerson).isNotNull();
         assertThat(updatedPerson.getFirstName()).isEqualTo("Willy");
-        assertThat(updatedPerson.getLastName()).isEqualTo("Roelofsen");
-        assertThat(updatedPerson.getDateOfBirth()).isEqualTo(LocalDate.parse("1986-03-15"));
+        assertThat(updatedPerson.getLastName()).isEqualTo("Johnson");
+        assertThat(updatedPerson.getDateOfBirth()).isEqualTo(LocalDate.parse("1990-04-26"));
         assertThat(updatedPerson.getGender()).isEqualTo(Gender.MALE);
     }
 
