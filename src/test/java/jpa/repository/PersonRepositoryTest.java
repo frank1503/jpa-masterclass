e75a0f10f288d3ef970283b0ce7a1ebaa64ea3f2
FrankRinkens <frank.rinkens@group9.nl>
2025-08-09 16:28:09 +0200

    opdracht-11-oplossing: fix test

--- src/test/java/jpa/repository/PersonRepositoryTest.java
+++ src/test/java/jpa/repository/PersonRepositoryTest.java
@@ -63,7 +63,7 @@ class PersonRepositoryTest {
         assertThat(createdPerson.getDateOfBirth()).isEqualTo(LocalDate.parse("1986-03-15"));
         assertThat(createdPerson.getGender()).isEqualTo(Gender.MALE);
         assertThat(createdPerson.getTelephoneNumbers()).hasSize(2).contains("0629731948", "0645859845");
-        assertThat(createdPerson.getAge()).isEqualTo(37);
+        assertThat(createdPerson.getAge()).isEqualTo(39);
         assertThat(createdPerson.getInsurances()).hasSize(2)
                 .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id", "person")
                 .contains(carInsurance, houseInsurance);
